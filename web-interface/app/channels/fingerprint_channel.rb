cc2b3a03124ff0bf0dd46208babcc20725bc2b07
hassanalubeidi <hassanalubeidi1996@gmail.com>
2018-04-01 08:14:18 +0400

    Created intial websocket channel

---
+++ web-interface/app/channels/fingerprint_channel.rb
@@ -0,0 +1,12 @@
+class FingerprintChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def scan
+  end
+end
