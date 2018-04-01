de386b0a92ee586635eee07e15ea9c4fb0c669f2
hassanalubeidi <hassanalubeidi1996@gmail.com>
2018-04-01 08:29:11 +0400

    Add broadcast behaviour to scan method

--- web-interface/app/channels/fingerprint_channel.rb
+++ web-interface/app/channels/fingerprint_channel.rb
@@ -1,12 +1,14 @@
 class FingerprintChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    stream_from "fingerprint_channel"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def scan
+  def scan(data)
+  	ActionCable.server.broadcast('messages',
+      message: render_message(data['message']))
   end
 end
