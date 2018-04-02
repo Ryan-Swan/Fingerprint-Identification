0f321e65f9725d2221708ae511cf9e84b53254c9
hassanalubeidi <hassanalubeidi1996@gmail.com>
2018-04-03 03:26:51 +0400

    initial booking management look

--- web-interface/app/assets/javascripts/channels/fingerprint.js
+++ web-interface/app/assets/javascripts/channels/fingerprint.js
@@ -1,3 +1,44 @@
+App.fingerprint = App.cable.subscriptions.create("FingerprintChannel", {
+  connected() {},
+    // Called when the subscription is ready for use on the server
+
+  disconnected() {},
+    // Called when the subscription has been terminated by the server
+
+  received(data) {
+    
+    if(data['message'] == "Scanned fingerprint") {
+    	$(".waiting-for-fingerprint").hide()
+    	$(".waiting-for-match").show()
+    	$(".waiting").show()
+        $(".loader").show()
+        document.getElementsByTagName("img")[0].src= ""
+    	document.getElementsByTagName("img")[0].src= "/assets/outputimage.png?" + Date.now()
+	}        
+    else if(data['message'] == "Valid authentication") {
+        console.log("Valid!");
+        $(".door-locked").hide()
+        $(".door-unlocked").show()
+        $(".loader").hide()
+        $(".waiting").hide()
+        window.location = "/fingerprints"
+    }
+        
+    else if(data['message'] == "Invalid authentication") {
+        console.log("InValid!");
+        $(".door-unlocked").hide()
+        $(".door-locked").show()
+        $(".waiting").hide()
+        $(".loader").hide()
+    }
+  },
+
+  scan(message) {
+    return this.perform('scan', {message});
+  }
+}
+);
+
 function findGetParameter(parameterName) {
     var result = null,
         tmp = [];
@@ -10,42 +51,3 @@ function findGetParameter(parameterName) {
         });
     return result;
 }
-
-App.fingerprint = App.cable.subscriptions.create("FingerprintChannel", {
-  connected() {},
-    // Called when the subscription is ready for use on the server
-
-  disconnected() {},
-    // Called when the subscription has been terminated by the server
-
-  received(data) {
-    
-    if(data['message'] == "Scanned fingerprint")
-    	if(findGetParameter("scanned") != "true") {
-        	window.location = "/fingerprints?scanned=true&loading=true";
-    	}
-        else {
-        	document.getElementsByTagName("img")[0].src= "/assets/outputimage.png?" + Date.now()
-	}        
-    else if(data['message'] == "Valid authentication") {
-        console.log("Valid!");
-        $(".door-locked").hide()
-        $(".door-unlocked").show()
-        $(".loader").hide()
-        window.location = "/fingerprints"
-    }
-        
-    else if(data['message'] == "Invalid authentication") {
-        console.log("InValid!");
-        $(".door-unlocked").hide()
-        $(".door-locked").show()
-        $(".loader").hide()
-
-    }
-  },
-
-  scan(message) {
-    return this.perform('scan', {message});
-  }
-}
-);
--- web-interface/config/routes.rb
+++ web-interface/config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'bookings/index'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'bookings/delete'
+
   get 'fingerprints/', to: 'fingerprints#index'
   get 'home/index'
   mount ActionCable.server => '/cable'
